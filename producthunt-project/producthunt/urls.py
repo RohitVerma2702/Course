a3b522269da107ba636e75cd82a586adf70ea03e
Rohit <verma199727@gmail.com>
2019-05-03 14:39:09 +0530

    authentication

--- producthunt-project/producthunt/urls.py
+++ producthunt-project/producthunt/urls.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-import jobs.views
+from products import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', jobs.views.home, name='home'),
-    path('blog/', include('blog.urls')),
+    path('', views.homepage, name='home'),
+    path('accounts/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
