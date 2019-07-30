7cd29dac59121a1b61b878f0b73224fb1d35c261
Rohit <verma199727@gmail.com>
2019-07-30 19:50:01 +0530

    producthunt working

--- producthunt-project/producthunt/settings.py
+++ producthunt-project/producthunt/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'producthuntdb',
         'USER': 'postgres',
-        'PASSWORD': 'Rohit@8852',
+        'PASSWORD': 'rohit',
         'HOST': 'localhost',
         'PORT': '5432',
     }
